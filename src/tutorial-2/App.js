def59949dc23ba8422cd00ed59e88bffcb75ff5f
compaeasy <compaeasy@yandex.ru>
2023-03-21 21:55:53 +0300

    Сделал урок #2

---
+++ src/tutorial-2/App.js
@@ -0,0 +1,12 @@
+import { InfoBlock } from './components/InfoBlock';
+
+function App() {
+  return (
+    <div>
+      <h2>Урок №2. Выполнил полностью</h2>
+      <InfoBlock />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/tutorial-2/components/InfoBlock.jsx
@@ -0,0 +1,11 @@
+export const InfoBlock = () => {
+  return (
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias fuga nostrum beatae
+      nesciunt, delectus earum numquam et illo porro maxime, dolorum iusto eaque eligendi dolore
+      commodi ducimus ad. Ducimus, doloremque.
+    </p>
+  );
+};
+
+export { InfoBlock };
