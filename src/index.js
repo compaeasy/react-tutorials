7b62d0393314878f3b414c209792d8986b736b9e
compaeasy <compaeasy@yandex.ru>
2023-03-22 22:52:31 +0300

    git

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './tutorial-1/App';
+// import App from './tutorial-1/App';
+import App from './tutorial-3/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/tutorial-3/components/Infoblock.jsx
+++ src/tutorial-3/components/Infoblock.jsx
@@ -7,5 +7,3 @@ export const InfoBlock = () => {
     </p>
   );
 };
-
-
